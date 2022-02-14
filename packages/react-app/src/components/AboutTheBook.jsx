4e72db4f0729a5726df5de07f62a858a631d82ee
Justin Pulley <justinpulley92@gmail.com>
2022-02-14 03:11:58 -0600

    Move hand over bar, arrange buttons, proper text styling, place book image.

--- packages/react-app/src/components/AboutTheBook.jsx
+++ packages/react-app/src/components/AboutTheBook.jsx
@@ -4,59 +4,58 @@ export default function AboutTheBook() {
   return (
     <>
       <div className="container px-5 mx-auto mt-10">
-        <h1>About the book</h1>
-        <h1>
-          These writings, collected from his essays before and during the rise of Ethereum, reveal Buterin to be a vivid
-          and imaginative writer, with context from media studies scholar Nathan Schneider.
-        </h1>
-        <h1>
-          While many around him were focused on seeing the value of their tokens rise, he was working through the
-          problems and possibilities of crafting an Internet-native world.
-        </h1>
+        <h6>Get Your Copy</h6>
+
+        <h3>
+          Proof of Stake will be available in September 2022. Until then, make a Gitcoin Pledge to fund public goods,
+          receiving a digital copy signed by Vitalik (and an optional NFT). Or, order the physical book (includes a
+          signed bookplate)
+        </h3>
       </div>
-      <div className="container px-5 mx-auto mt-10">
+      <div className="container px-5 mx-auto mt-20">
         <div className="flex flex-wrap -m-4">
-          <div className="w-1/3 lg:mb-0 p-4">
-            <div className="h-full text-center">
-              <a href="" target="_blank" rel="noreferrer">
-                <img alt="twitter" className="h-10 w-10 mb-8 object-center inline-block" src="assets/Pledge.svg" />
-              </a>
-            </div>
-          </div>
-          <div className="w-1/3 lg:mb-0 p-4">
-            <div className="h-full text-center">
+          <div className="w-1/3 lg:mb-0 ">
+            <div className="h-3/4 text-center">
               <a href="" target="_blank" rel="noreferrer">
                 <img
-                  alt="discord"
-                  className="h-10 w-10 mb-8 -mt-2 object-center inline-block"
-                  src="assets/Download.svg"
-                />
-              </a>
-            </div>
-          </div>
-          <div className="w-1/3 lg:mb-0 p-4">
-            <div className="h-full text-center">
-              <a href="" target="_blank" rel="noreferrer">
-                <img
-                  alt="discord"
-                  className="h-10 w-10 mb-8 object-center inline-block"
+                  alt="twitter"
+                  className="h-3/4 w-50 mb-8 object-center inline-block"
                   src="assets/Physical_copy.svg"
                 />
               </a>
             </div>
           </div>
+          <div className="w-1/3 lg:mb-0 ">
+            <div className="h-full text-center">
+              <a href="" target="_blank" rel="noreferrer">
+                <img
+                  alt="discord"
+                  className="h-3/4 w-50 mb-8 -mt-2 object-center inline-block"
+                  src="assets/Pledge.svg"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="w-1/3 lg:mb-0 ">
+            <div className="h-3/4 text-center">
+              <a href="" target="_blank" rel="noreferrer">
+                <img alt="discord" className="h-3/4 w-50 mb-8 object-center inline-block" src="assets/Download.svg" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="container px-5 mx-auto mt-10">
-        <h1>About the book</h1>
-        <h1>
+      <div className="container px-5 mx-auto">
+        <h6>About the book</h6>
+        <h3>
           These writings, collected from his essays before and during the rise of Ethereum, reveal Buterin to be a vivid
           and imaginative writer, with context from media studies scholar Nathan Schneider.
-        </h1>
-        <h1>
+        </h3>
+        <br />
+        <h3>
           While many around him were focused on seeing the value of their tokens rise, he was working through the
           problems and possibilities of crafting an Internet-native world.
-        </h1>
+        </h3>
       </div>
     </>
   );
