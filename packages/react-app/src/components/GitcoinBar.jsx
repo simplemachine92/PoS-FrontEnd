7c0518cb26c0ac259fd290d282ad4e51a0db3c8c
farque65 <fahim.farque@gmail.com>
2022-02-14 22:38:12 -0500

    Text style updates

--- packages/react-app/src/components/GitcoinBar.jsx
+++ packages/react-app/src/components/GitcoinBar.jsx
@@ -4,9 +4,9 @@ export default function GitcoinBar() {
   return (
     // Bar should be fitting screen, hand may need to be repositioned but heres a start.
     <div className="container relative mx-auto mt-10">
-      <img alt="discord" className="mb-8 -mt-2 object-fill " src="assets/Gitcoinbar.svg" />
+      <img alt="Gitcoin Message" className="mb-8 -mt-2 object-fill max-w-full" src="assets/Gitcoinbar.svg" />
       <img
-        alt="discord"
+        alt="Ethereum Hand"
         className="absolute -bottom-0 -right-12 w-1/2 object-scale-down inline-block"
         src="assets/EthereumHand.gif"
       />
--- packages/react-app/src/views/Pledge.jsx
+++ packages/react-app/src/views/Pledge.jsx
@@ -32,95 +32,93 @@ function Pledge({ writeContracts, tx, address, loadWeb3Modal }) {
   const [uValue, setU] = useState("0.0001337");
 
   return (
-    <>
-      <div>
-        {/* <h3 class="title">Take the Green Pill:</h3>
+    <div className="font-spacemono">
+      {/* <h3 class="title">Take the Green Pill:</h3>
           <h3 class="title">Regenerative CryptoEconomics</h3> */}
 
-        <form action="/order">
-          <div className="hero-container">
-            <img src="image/reee.svg" />
-            {/* Fix button redirect */}
+      <form action="/order" className="mb-10">
+        <div className="hero-container">
+          <img src="image/reee.svg" />
+          {/* Fix button redirect */}
 
-            <button class="btn">Pre-order</button>
-          </div>
-        </form>
-        <footer className="footer-container background-greenpill">
-          <section className="green-pill-form-section">
-            <div>
-              <h5>Make a Pledge</h5>
+          <button className="bg-yellow-pos px-32 py-8 text-green-posblue text-3xl">Pre-order</button>
+        </div>
+      </form>
+      <footer className="footer-container background-greenpill">
+        <section className="green-pill-form-section">
+          <div>
+            <h5 className="text-yellow-pos text-3xl font-bold">Make a Pledge</h5>
 
-              <h4 className="p-8 text-white">
-                Receive a digitally signed book plate (with your full digital copy upon release) and an optional to mint
-                "Soulbound" NFT 👀
-              </h4>
+            <h4 className="p-8 text-white">
+              Receive a digitally signed book plate (with your full digital copy upon release) and an optional to mint
+              "Soulbound" NFT 👀
+            </h4>
 
-              <InputNumber
-                className="form"
-                style={{ width: "80%", margin: "auto" }}
-                bordered={false}
-                controls={false}
-                onChange={e => {
-                  const currValue = String(e);
-                  setU(currValue);
-                  console.log("changin", uValue);
-                }}
-                step={0.1}
-                defaultValue={0.0001337}
-                onPressEnter={async () => {
+            <InputNumber
+              className="form"
+              style={{ width: "80%", margin: "auto" }}
+              bordered={false}
+              controls={false}
+              onChange={e => {
+                const currValue = String(e);
+                setU(currValue);
+                console.log("changin", uValue);
+              }}
+              step={0.1}
+              defaultValue={0.0001337}
+              onPressEnter={async () => {
+                try {
+                  const txCur = await tx(writeContracts.GreenPill_Pages.pledge({ value: utils.parseEther(uValue) }));
+                  await txCur.wait();
+                } catch {
+                  console.log("pledge failed");
+                }
+              }}
+            />
+            {address ? (
+              <StyledButton
+                type="buttons-green-pill"
+                className=""
+                style={{ marginTop: 40, width: "30%" }}
+                onClick={async () => {
                   try {
-                    const txCur = await tx(writeContracts.GreenPill_Pages.pledge({ value: utils.parseEther(uValue) }));
+                    const txCur = await tx(
+                      writeContracts.ProofOfStake_Pages.pledge({ value: utils.parseEther(uValue) }),
+                    );
                     await txCur.wait();
                   } catch {
-                    console.log("pledge failed");
+                    console.log("button pledge failed");
                   }
                 }}
-              />
-              {address ? (
-                <StyledButton
-                  type="buttons-green-pill"
-                  className=""
-                  style={{ marginTop: 40, width: "30%" }}
-                  onClick={async () => {
-                    try {
-                      const txCur = await tx(
-                        writeContracts.ProofOfStake_Pages.pledge({ value: utils.parseEther(uValue) }),
-                      );
-                      await txCur.wait();
-                    } catch {
-                      console.log("button pledge failed");
-                    }
-                  }}
-                >
-                  Submit
-                </StyledButton>
-              ) : (
-                <StyledButton style={{ marginTop: 40, width: "30%" }} onClick={loadWeb3Modal}>
-                  Connect to pledge
-                </StyledButton>
-              )}
-            </div>
-          </section>
-
-          <div>
-            <p className="p-10 italic">
-              For pledges made here, 90% of funds go to the Gitcoin Grants matching pool and 10% to our publishing
-              partner, Seven Stories Press, for supporting public goods in independent publishing. For book copies
-              purchased here or anywhere, Vitalik has committed all of his proceeds to Gitcoin Grants.
-            </p>
+              >
+                Submit
+              </StyledButton>
+            ) : (
+              <StyledButton style={{ marginTop: 40, width: "30%" }} onClick={loadWeb3Modal}>
+                Connect to pledge
+              </StyledButton>
+            )}
           </div>
+        </section>
 
-          <div className="gitcoin-brand">
-            <a href="https://gitcoindao.com">
-              <img src="/image/gitcoindao_sign.svg" alt="gitcoinDAO" />
-            </a>
-            {""}
-            {""}
-            &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; <a href="https://t.me/+g9TM8i7GpxAzMGUx">Join the Telegram</a>
-          </div>
-        </footer>
-      </div>
-    </>
+        <div>
+          <p className="p-10 italic">
+            For pledges made here, 90% of funds go to the Gitcoin Grants matching pool and 10% to our publishing
+            partner, Seven Stories Press, for supporting public goods in independent publishing. For book copies
+            purchased here or anywhere, Vitalik has committed all of his proceeds to Gitcoin Grants.
+          </p>
+        </div>
+
+        <div className="gitcoin-brand">
+          <a href="https://gitcoindao.com">
+            <img src="/image/gitcoindao_sign.svg" alt="gitcoinDAO" />
+          </a>
+          {""}
+          {""}
+          &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; <a href="https://t.me/+g9TM8i7GpxAzMGUx">Join the Telegram</a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
