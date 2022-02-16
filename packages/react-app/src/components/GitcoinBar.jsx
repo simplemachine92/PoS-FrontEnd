1f0880ac78c2c4315c74876751914283226f2139
farque65 <fahim.farque@gmail.com>
2022-02-15 22:47:28 -0500

    Home Page Update

--- packages/react-app/src/components/GitcoinBar.jsx
+++ packages/react-app/src/components/GitcoinBar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function GitcoinBar() {
   return (
     // Bar should be fitting screen, hand may need to be repositioned but heres a start.
-    <div className="container relative mx-auto mt-10">
-      <img alt="Gitcoin Message" className="mb-8 -mt-2 object-fill max-w-full" src="assets/Gitcoinbar.svg" />
+    <div className="container relative max-w-full">
+      <img alt="Gitcoin Message" className="mb-8 -mt-2 object-fill w-full" src="assets/Gitcoinbar.svg" />
       <img
         alt="Ethereum Hand"
         className="absolute -bottom-0 -right-12 w-1/2 object-scale-down inline-block"
--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -5,19 +5,21 @@ import React from "react";
 export default function IntroHeader() {
   return (
     // We need proper resizing on book image, button, and fix the "Coming Soon" styling
-    <div className="flex flex-wrap mt-10 bg-headerBackground bg-no-repeat">
+    <div className="flex flex-wrap bg-headerBackground bg-no-repeat">
       <div className="flex flex-wrap w-1/2 justify-center items-center">
-        <div className="max-w-md  py-10 px-20 backdrop-filter backdrop-blur-lg">
-          <div class="text-center mt-4 backdrop-blur-md">
-            <img alt="Book" className="mb-8 -mt-2 object-scale-down " src="assets/book.svg" />
-            <a
-              href="https://www.google.com"
-              target="_blank"
-              rel="noreferrer"
-              className="mt-10 font-spacemono bg-yellow-300 px-8 py-4 text-xl text-gray-900 font-semibold rounded hover:text-white text-base"
-            >
-              Pre-order
-            </a>
+        <div className="max-w-md py-10 px-10">
+          <div className="py-10 px-10 backdrop-filter backdrop-blur-xl rounded-lg">
+            <div class="text-center mt-4 backdrop-blur-md">
+              <img alt="Book" className="mb-8 -mt-2 object-scale-down " src="assets/book.svg" />
+              <a
+                href="https://www.google.com"
+                target="_blank"
+                rel="noreferrer"
+                className="mt-10 font-spacemono bg-yellow-300 px-20 py-4 text-lg text-gray-900 font-semibold rounded hover:text-white"
+              >
+                Pre-order
+              </a>
+            </div>
           </div>
         </div>
       </div>
