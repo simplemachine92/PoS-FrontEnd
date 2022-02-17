20ee53d110a13d16b1377bef31014bc21b334481
Justin Pulley <justinpulley92@gmail.com>
2022-02-16 21:21:08 -0700

    final header fix.

--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -7,19 +7,17 @@ export default function IntroHeader() {
     // We need proper resizing on book image, button, and fix the "Coming Soon" styling
     <div className="flex flex-wrap bg-ringsBackground bg-no-repeat bg-right bg-auto">
       <div className="flex flex-wrap w-1/2 justify-center bg-no-repeat items-center">
-        <div className="max-w-md py-10 px-10">
-          <div className="py-10 px-10 backdrop-filter rounded-lg">
-            <div className="text-center">
-              <img alt="Book" className="mb-8 ml-1" src="assets/book_and_shadow.svg" />
-              <button
-                class="font-spacemono px-12 py-2 mr-10 bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-                type="btn btn-primary"
-              >
-                <a href="https://www.boulderbookstore.net/product/proof" class="btn btn-primary">
-                  Pre-Order
-                </a>
-              </button>
-            </div>
+        <div className="max-w-md py-10">
+          <div className="py-10 backdrop-filter rounded-lg content-center">
+            <img alt="Book" className="mb-8 ml-7" src="assets/book_and_shadow.svg" />
+            <button
+              class="px-14 ml-6 py-2 bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              type="btn btn-primary"
+            >
+              <a href="https://www.boulderbookstore.net/product/proof" class="btn btn-primary">
+                Pre-Order
+              </a>
+            </button>
           </div>
         </div>
       </div>
