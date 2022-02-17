29aaaab6c3e5fc4e636ba89e77439eff83083b0c
Justin Pulley <justinpulley92@gmail.com>
2022-02-16 20:41:32 -0700

    Header fix.

--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -6,14 +6,14 @@ export default function IntroHeader() {
   return (
     // We need proper resizing on book image, button, and fix the "Coming Soon" styling
     <div className="flex flex-wrap bg-ringsBackground bg-no-repeat bg-right bg-auto">
-      <div className="flex flex-wrap w-1/2 justify-center bg-headerBackground bg-cover bg-no-repeat items-center">
+      <div className="flex flex-wrap w-1/2 justify-center bg-no-repeat items-center">
         <div className="max-w-md py-10 px-10">
-          <div className="py-10 px-10 backdrop-filter backdrop-blur-xl rounded-lg">
-            <div className="text-center mt-4 backdrop-blur-md">
+          <div className="py-10 px-10 backdrop-filter rounded-lg">
+            <div className="text-center">
               <img alt="Book" className="mb-8 ml-1" src="assets/book_and_shadow.svg" />
               <button
                 href="https://www.boulderbookstore.net/product/proof"
-                class="font-spacemono px-12 py-2 mr-10 font-semibold bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                class="font-spacemono px-12 py-2 mr-10 bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                 type="button"
               >
                 Pre-order
@@ -22,11 +22,8 @@ export default function IntroHeader() {
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap w-1/2 pl-16">
-        <div className="md:w-1/2 md:pr-12 md:py-8">
-          <img alt="Title" className="mb-8" src="assets/proof_of_stake_home_title.svg" />
-          <p className="leading-relaxed text-base text-gray-700 font-spacemono float-right">By Vitalik Buterin</p>
-        </div>
+      <div className="flex flex-wrap w-1/3">
+        <img class="shadow" className="mb-8 object-fit" src="assets/name.svg" />
       </div>
     </div>
   );
