1cdd3473d28a3a778b9a46de0c975caf910877ed
farque65 <fahim.farque@gmail.com>
2022-02-16 20:16:30 -0500

    Updated Preorder Button

--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -9,24 +9,23 @@ export default function IntroHeader() {
       <div className="flex flex-wrap w-1/2 justify-center bg-headerBackground bg-cover bg-no-repeat items-center">
         <div className="max-w-md py-10 px-10">
           <div className="py-10 px-10 backdrop-filter backdrop-blur-xl rounded-lg">
-            <div class="text-center mt-4 backdrop-blur-md">
+            <div className="text-center mt-4 backdrop-blur-md">
               <img alt="Book" className="mb-8 ml-1" src="assets/book_and_shadow.svg" />
-              <a
+              <button
                 href="https://www.boulderbookstore.net/product/proof"
-                target="_blank"
-                rel="noreferrer"
-                className="mt-10 font-spacemono bg-yellow-300 px-10 py-2 mr-10 text-lg text-gray-900 font-semibold rounded hover:text-white"
+                class="font-spacemono px-12 py-2 mr-10 font-semibold bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="button"
               >
                 Pre-order
-              </a>
+              </button>
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-wrap w-1/2 pl-16">
-        <div class="md:w-1/2 md:pr-12 md:py-8">
+        <div className="md:w-1/2 md:pr-12 md:py-8">
           <img alt="Title" className="mb-8" src="assets/proof_of_stake_home_title.svg" />
-          <p class="leading-relaxed text-base text-gray-700 font-spacemono float-right">By Vitalik Buterin</p>
+          <p className="leading-relaxed text-base text-gray-700 font-spacemono float-right">By Vitalik Buterin</p>
         </div>
       </div>
     </div>
