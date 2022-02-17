fa25401dfbe7e61dd46d7e46c259327284dbdc33
Justin Pulley <justinpulley92@gmail.com>
2022-02-17 07:47:34 -0700

    Finally again lool.

--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -10,7 +10,7 @@ export default function IntroHeader() {
         <div className="max-w-md py-10">
           <div className="py-0 backdrop-filter rounded-lg content-center">
             <img alt="Book" className="mb-8 ml-7" src="assets/book_and_shadow.svg" />
-            <form action="https://www.boulderbookstore.net/product/proof">
+            <form action="/order">
               <button
                 class="px-14 ml-6 py-2 bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                 type="btn btn-primary"
