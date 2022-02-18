a4b167450b6442a5bbc99eabb315e08872011e4a
Justin Pulley <justinpulley92@gmail.com>
2022-02-18 15:09:24 -0700

    readme progress, fixed page width, sample env.

--- packages/react-app/src/components/GitcoinBar.jsx
+++ packages/react-app/src/components/GitcoinBar.jsx
@@ -7,7 +7,7 @@ export default function GitcoinBar() {
       <img alt="Gitcoin Message" className="mb-8 -mt-2 object-fill w-full" src="assets/Content.svg" />
       <img
         alt="Ethereum Hand"
-        className="absolute -bottom-0 -right-12 w-1/2 object-scale-down inline-block"
+        className="absolute -bottom-0 w-1/2 object-scale-down inline-block"
         src="assets/EthereumHand.gif"
       />
     </div>
