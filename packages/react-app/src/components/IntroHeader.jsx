4497d520b321ba8332733beacb3aad2d36af4b03
farque65 <fahim.farque@gmail.com>
2022-02-21 21:09:28 -0500

    Updated Header Text Responsiveness

--- packages/react-app/src/components/IntroHeader.jsx
+++ packages/react-app/src/components/IntroHeader.jsx
@@ -12,10 +12,10 @@ export default function IntroHeader() {
       <div className="flex flex-wrap w-1/2 justify-center bg-no-repeat items-center">
         <div className="max-w-md py-10">
           <div className="py-0 backdrop-filter rounded-lg content-center">
-            <img alt="Book" className="mb-8 ml-7" src="assets/book_and_shadow.svg" />
+            <img alt="Book" className="mb-2 md:mb-8 ml-7" src="assets/book_and_shadow.svg" />
             <form action="/order">
               <button
-                class="px-14 ml-6 py-2 bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                className="ml-7 w-3/4 py-2 md:py-4 text-base md:text-xl bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                 type="btn btn-primary"
               >
                 Pre-Order
@@ -24,8 +24,8 @@ export default function IntroHeader() {
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap w-1/3">
-        <img class="shadow" className="mb-8 object-fit" src="assets/name.svg" />
+      <div className="flex flex-wrap w-1/2 md:w-1/3 pr-4 md:pr-2">
+        <img className="book and shadow" className="mb-8 object-fit" src="assets/name.svg" />
       </div>
     </div>
   );
