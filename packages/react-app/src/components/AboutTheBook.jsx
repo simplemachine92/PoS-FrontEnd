80e576767895a7cb5a23372841c3248a7f8cba95
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2022-02-22 13:32:17 +0530

    remove w-50 from styling in aboutBook.js

--- packages/react-app/src/components/AboutTheBook.jsx
+++ packages/react-app/src/components/AboutTheBook.jsx
@@ -17,14 +17,14 @@ export default function AboutTheBook() {
           <div className="w-1/3 lg:mb-0 ">
             <div className="h-3/4 text-center">
               <a href="/order" target="_blank" rel="noreferrer">
-                <img className="h-3/4 w-50 mb-8 object-center inline-block" src="assets/Physical_copy1.svg" />
+                <img className="h-3/4 mb-8 object-center inline-block" src="assets/Physical_copy1.svg" />
               </a>
             </div>
           </div>
           <div className="w-1/3 lg:mb-0 ">
             <div className="h-full text-center">
               <a href="/pledge" target="_blank" rel="noreferrer">
-                <img className="h-3/4 w-50 -mt-2 object-center inline-block" src="assets/Pledge1.svg" />
+                <img className="h-3/4 -mt-2 object-center inline-block" src="assets/Pledge1.svg" />
                 <br />
                 <p className="text-center mx-auto text-white italic text-xs">
                   For pledges made here, 90% of funds go to the Gitcoin Grants matching pool and 10% to our publishing
