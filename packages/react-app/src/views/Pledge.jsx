cb145d191b9f6c5b4f7a4ded6d33bca6b0a1d1cc
Justin Pulley <justinpulley92@gmail.com>
2022-03-10 07:30:14 -0600

    Email form rough v, revert contract import, requested changes.

--- packages/react-app/src/views/Pledge.jsx
+++ packages/react-app/src/views/Pledge.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import React, { useState } from "react";
-import { Button, InputNumber } from "antd";
+import { Button, InputNumber, Input } from "antd";
 import { Footer, Quotes } from "../components";
-import { Redirect } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, get, child } from "firebase/database";
 
 const { utils, BigNumber } = require("ethers");
 
@@ -22,16 +23,64 @@ export const StyledButton = styled(Button)`
   }
 `;
 
+export const StyledInput = styled(Input)`
+  width: 100%;
+  height: 80px;
+  margin-top: 5px;
+  background: #31dbe5;
+  border: 1px solid #207191;
+  border-radius: 8px;
+  outline: none;
+  text-align: center;
+  font-size: 30px;
+  font-size: 2.5vw;
+  font-family: Space Mono, sans-serif;
+  color: #ffffff;
+  background: #31dbe5;
+
+  &::placeholder {
+    margin-top: 100px;
+    color: #ffffff;
+    font-family: Space Mono, sans-serif;
+    font-style: normal;
+    font-size: 20px;
+    text-align: center;
+  },
+`;
+
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
  * @param {*} yourLocalBalance balance on current network
  * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
  * @returns react component
  */
-function Pledge({ firebaseConfig, writeContracts, tx, address, loadWeb3Modal }) {
+function Pledge({ writeContracts, tx, address, loadWeb3Modal }) {
   // you can also use hooks locally in your component of choice
   // in this case, let's keep track of 'purpose' variable from our contract
   const [uValue, setU] = useState("0.01337");
+  const [eValue, setE] = useState();
+
+  // Initialize Firebase
+
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: "https://proofofstake-91004.firebaseio.com/",
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  // Get a reference to the database service
+  const database = getDatabase(app);
+
+  /* function getString() {
+    console.log("kms", );
+    return String(eValue);
+  } */
 
   return (
     <>
@@ -58,19 +107,17 @@ function Pledge({ firebaseConfig, writeContracts, tx, address, loadWeb3Modal }) 
             </div>
 
         <footer className="">
-          <section className="green-pill-form-section">
+          
             <div>
               <h5 className="font-bold mt-6">Make a Donation</h5>
 
               <h3 className="text-sm p-10">
-                You could receive a digitally signed book plate from Vitalik.eth (his discretion)
+              Pledge any amount of ETH below to request a digitally signed book plate from Vitalik. Enter your email to be notified when the digital book is available.
               </h3>
 
               <InputNumber
-                className="form"
-                style={{ width: "80%", margin: "auto" }}
+                className="form w-3/4 mx-auto my-auto"
                 bordered={false}
-                controls={false}
                 onChange={e => {
                   const currValue = String(e);
                   setU(currValue);
@@ -86,6 +133,21 @@ function Pledge({ firebaseConfig, writeContracts, tx, address, loadWeb3Modal }) 
                   } catch {}
                 }}
               />
+                <StyledInput
+                className=" w-3/4 mx-auto mt-5"
+                maxLength={320}
+                placeholder="email"
+                onChange={f => {
+                  const currValue2 = f;
+                  setE(currValue2);
+                }}
+                onPressEnter={() => {const db = database;
+                  set(ref(db, `PoS/` + address), {
+                   email: eValue.target.value
+                  });
+                  }}
+              />
+              
               {address ? (
                 <button
                 type="btn btn-primary"
@@ -93,11 +155,18 @@ function Pledge({ firebaseConfig, writeContracts, tx, address, loadWeb3Modal }) 
                   style={{ marginTop: 40, width: "30%" }}
                   onClick={async () => {
                     try {
+
+                      const db = database;
+                      set(ref(db, `PoS/` + address), {
+                       email: eValue.target.value
+                      });
+
                       const txCur = await tx(
                         writeContracts.ProofOfStake_Pages.pledge({ value: utils.parseEther(uValue) }),
                       );
                       await txCur.wait();
                       // We need a redirect to /complete, this isnt working.
+
                       //return <Redirect to="/complete" />;
                     } catch {
                       console.log("button pledge failed");
@@ -107,12 +176,18 @@ function Pledge({ firebaseConfig, writeContracts, tx, address, loadWeb3Modal }) 
                   Submit
                 </button>
               ) : (
-                <StyledButton style={{ marginTop: 40, width: "30%" }} onClick={loadWeb3Modal}>
+                
+                <button
+                type="btn btn-primary"
+                  className="w-3/4 py-2 px-2 mt-4 mx-auto sm:py-4 sm:px-3 text-sm md:text-lg bg-gradient-to-r from-yellow-300 to-yellow-pos hover:from-yellow-pos hover:to-yellow-poslight text-gray-900 font-bold rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                  style={{ marginTop: 40, width: "30%" }}
+                  onClick={loadWeb3Modal}
+                >
                   Connect
-                </StyledButton>
+                </button>
               )}
             </div>
-          </section>
+          
 
           <div>
             <br />
