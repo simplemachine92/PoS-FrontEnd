a3627d369d50fe9fca8755c9f4f3ca3150163784
simp92 <justinpulley92@gmail.com>
2022-04-04 11:44:15 -0500

    Contract constructor update with original pledgees.

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -20,7 +20,29 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   await deploy("ProofOfStake_Pages", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    //args: ["0x16f5a35647d6f03d5d3da7b35409d65ba03af3b2"],
+    args: [[
+      "0xb010ca9Be09C382A9f31b79493bb232bCC319f01",
+      "0x09E08a6c3015FA2c0DBafF4Ee2EB659Efd9fe31c",
+      "0xB0CCf43adA6CBaA26dcf4907117b496d49f74242",
+      "0x8913b99d5efD9e391AdE59168230F6425813D90A",
+      "0x1A50a6238eb67285cCD4DF17f75CCe430BAAE2A4",
+      "0xA4ca1b15fE81F57cb2d3f686c7B13309906cd37B",
+      "0xBFc7CAE0Fad9B346270Ae8fde24827D2D779eF07",
+      "0x69d95FfCeCe36DC3f2e9f5e42EB270a0BcD98665",
+      "0x2fD39e6741b1446C51e8A120aB1D69F645e10e0E",
+      "0x149986E1b481D9E026A3C4F472C0B86A7D5DF08D"
+    ], [
+      "133700000000000020",
+      "133800000000000000",
+      "133700200000000000",
+      "133700200000000000",
+      "133700200000000000",
+      "133700200000000000",
+      "133700000000000000",
+      "133700200000000000",
+      "133700000000000000",
+      "133700000000000000"
+    ]],
     log: true,
   });
 
