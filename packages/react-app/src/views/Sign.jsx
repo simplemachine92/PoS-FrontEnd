af79f866317e9d9732dc1bc190cdd49682082b8f
simp92 <justinpulley92@gmail.com>
2022-04-04 10:27:45 -0500

    Formatting.

--- packages/react-app/src/views/Sign.jsx
+++ packages/react-app/src/views/Sign.jsx
@@ -16,7 +16,7 @@ import {
   Form,
   List,
   Pagination,
-  Table
+  Table,
 } from "antd";
 import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
@@ -53,31 +53,31 @@ export default function Signator({
 
   const FilterByNameInput2 = (
     <Input
-    className="w-2/3"
+      className="w-2/3"
       placeholder="Sort by ENS or 0x.."
       value={value2}
       onChange={e => {
-        console.log("curr", e.target.value)
-        
+        console.log("curr", e.target.value);
+
         const currValue = e.target.value;
         setValue2(currValue);
         const filteredData = events.filter(entry => entry.args[0].includes(currValue));
         setDataSource2(filteredData);
-      
-        // Check if an input ENS resolves
-        if (e.target.value.startsWith("0")) {} else {
-        mainnetProvider.resolveName(e.target.value).then(function(address2) {
-          console.log("Address: " + address2);
-          if (address2 == null) {
-            console.log("No record for this ENS")
-            setDataSource2(events);
-            } else {
-              const filteredData2 = events.filter(entry => entry.args[0].includes(address2))
-              setDataSource2(filteredData2);
-            };
-          });
-        /* console.log("ensName", ensName) */
 
+        // Check if an input ENS resolves
+        if (e.target.value.startsWith("0")) {
+        } else {
+          mainnetProvider.resolveName(e.target.value).then(function (address2) {
+            console.log("Address: " + address2);
+            if (address2 == null) {
+              console.log("No record for this ENS");
+              setDataSource2(events);
+            } else {
+              const filteredData2 = events.filter(entry => entry.args[0].includes(address2));
+              setDataSource2(filteredData2);
+            }
+          });
+          /* console.log("ensName", ensName) */
         }
       }}
     />
@@ -103,7 +103,6 @@ export default function Signator({
       sortDirections: ["ascend", "descend"],
     },
   ];
-
 
   //prettier-ignore
   const eip712Example = {
@@ -250,7 +249,7 @@ export default function Signator({
         });
         console.log("dblist", dbList);
         if (dbList.length) {
-          events.forEach(pledge => {  
+          events.forEach(pledge => {
             eventList.push(pledge.args.pledgee);
             console.log("event list", eventList);
             objectList.push(pledge);
@@ -262,7 +261,7 @@ export default function Signator({
                 // push to to-do
                 toSign.push(objectList[x]);
                 setList(toSign);
-                console.log("aalist", toSign)
+                console.log("aalist", toSign);
               }
             }
           });
@@ -359,41 +358,42 @@ export default function Signator({
 
   const [select, setSelect] = useState({
     selectedRowKeys: [],
-    loading: false
+    loading: false,
   });
 
   const { selectedRowKeys, loading } = select;
 
   const rowSelection = {
     selectedRowKeys,
-    onChange: (selectedRowKeys) => {
-      console.log("sel", selectedRowKeys)
+    onChange: selectedRowKeys => {
+      console.log("sel", selectedRowKeys);
       setSelect({
         ...select,
-        selectedRowKeys: selectedRowKeys
+        selectedRowKeys: selectedRowKeys,
       });
-      console.log("eventc", events)
-      const filtered = Object.keys(events).filter(key => selectedRowKeys.includes(key))
-       .reduce((obj, key) => {
-        obj = events[key];
-        return obj;
-      }, {});
-      console.log("flt", filtered)
-       setList([filtered])
+      console.log("eventc", events);
+      const filtered = Object.keys(events)
+        .filter(key => selectedRowKeys.includes(key))
+        .reduce((obj, key) => {
+          obj = events[key];
+          return obj;
+        }, {});
+      console.log("flt", filtered);
+      setList([filtered]);
     },
-    type: 'radio',
+    type: "radio",
   };
 
-useEffect(async () => {
-  Object.keys(events).forEach(key => {
-    events[key].key = key;
-    console.log("wat3", key, events[key]);
-  });
-  setDataSource2(events);
-  setReady(true);
-  console.log("eventsS", events )
-  console.log("tosign", toSign)
-}, [list]);
+  useEffect(async () => {
+    Object.keys(events).forEach(key => {
+      events[key].key = key;
+      console.log("wat3", key, events[key]);
+    });
+    setDataSource2(events);
+    setReady(true);
+    console.log("eventsS", events);
+    console.log("tosign", toSign);
+  }, [list]);
 
   return (
     <div
@@ -407,135 +407,138 @@ useEffect(async () => {
     >
       {userReady ? (
         <>
-        <Row>
-          <Col span={12}>
-            <Card>
-              {type === "message" && (
-                <Input.TextArea
-                  style={{ fontSize: 18 }}
-                  size="large"
-                  autoSize={{ minRows: 1 }}
-                  value={messageText}
-                  onChange={e => {
-                    setMessageText(e.target.value);
-                  }}
-                />
-              )}
-              <h1>Queued / Selected:</h1>
-              <Space direction="vertical" style={{ width: "auto" }}>
-                <div
-                  style={{
-                    width: "10px auto",
-                    margin: "10px auto",
-                    paddingLeft: 20,
-                    paddingRight: 20,
-                    paddingBottom: 20,
-                  }}
-                >
-                  <List
-                    bordered
-                    dataSource={list}
-                    split={false}
-                    pagination={{
-                      defaultPageSize: "1",
-                      total: "1",
-                      hideOnSinglePage: true,
+          <Row>
+            <Col span={12}>
+              <Card>
+                {type === "message" && (
+                  <Input.TextArea
+                    style={{ fontSize: 18 }}
+                    size="large"
+                    autoSize={{ minRows: 1 }}
+                    value={messageText}
+                    onChange={e => {
+                      setMessageText(e.target.value);
                     }}
-                    renderItem={item => (
-                      <List.Item key={item}>
-                        <Address
-                          value={item.args[0]}
-                          ensProvider={mainnetProvider}
-                          fontSize={32}
-                          style={{ display: "flex", flex: 1, alignItems: "center" }}
-                        />
-                      </List.Item>
-                    )}
                   />
-                </div>
-              </Space>
-              {type === "typedData" && (
-                <>
-                  <h1>Message:</h1>
-                  <Space direction="vertical" style={{ width: "50%" }}>
-                    <Input.TextArea
-                      style={{ fontSize: 18 }}
-                      maxLength={60}
-                      showCount={true}
-                      size="large"
-                      autoSize={{ minRows: 1 }}
-                      value={messageText}
-                      onChange={e => {
-                        setMessageText(e.target.value);
+                )}
+                <h1>Queued / Selected:</h1>
+                <Space direction="vertical" style={{ width: "auto" }}>
+                  <div
+                    style={{
+                      width: "10px auto",
+                      margin: "10px auto",
+                      paddingLeft: 20,
+                      paddingRight: 20,
+                      paddingBottom: 20,
+                    }}
+                  >
+                    <List
+                      bordered
+                      dataSource={list}
+                      split={false}
+                      pagination={{
+                        defaultPageSize: "1",
+                        total: "1",
+                        hideOnSinglePage: true,
                       }}
+                      renderItem={item => (
+                        <List.Item key={item}>
+                          <Address
+                            value={item.args[0]}
+                            ensProvider={mainnetProvider}
+                            fontSize={32}
+                            style={{ display: "flex", flex: 1, alignItems: "center" }}
+                          />
+                        </List.Item>
+                      )}
                     />
-                    {invalidJson && <Alert message="Invalid Json" type="error" />}
-                    {/* typedDataChecks.domain===false&&<Alert message="No domain specified" type="info" /> */}
-                    {typedDataChecks.types === false && <Alert message="Missing types" type="error" />}
-                    {typedDataChecks.message === false && <Alert message="Missing message" type="error" />}
-                    {!invalidJson && !typedDataChecks.hash && (
-                      <Alert message="Invalid EIP-712 input data" type="error" />
-                    )}
-                  </Space>
-                </>
-              )}
-              <Collapse ghost></Collapse>
-              <Space>
-                <Button
-                  size="large"
-                  type="primary"
-                  onClick={action !== "sign" ? signMessage : injectedProvider ? signMessage : loadWeb3Modal}
-                  disabled={
-                    (type === "typedData" && (!typedDataChecks.hash || invalidJson)) ||
-                    (action === "verify" && (!ethers.utils.isAddress(manualAddress) || !manualSignature))
-                  }
-                  loading={signing}
-                  style={{ marginTop: 10 }}
-                >
-                  {action !== "sign" ? action : injectedProvider ? action : "Connect account to sign"}
-                </Button>
-                {signing && (
+                  </div>
+                </Space>
+                {type === "typedData" && (
+                  <>
+                    <h1>Message:</h1>
+                    <Space direction="vertical" style={{ width: "50%" }}>
+                      <Input.TextArea
+                        style={{ fontSize: 18 }}
+                        maxLength={60}
+                        showCount={true}
+                        size="large"
+                        autoSize={{ minRows: 1 }}
+                        value={messageText}
+                        onChange={e => {
+                          setMessageText(e.target.value);
+                        }}
+                      />
+                      {invalidJson && <Alert message="Invalid Json" type="error" />}
+                      {/* typedDataChecks.domain===false&&<Alert message="No domain specified" type="info" /> */}
+                      {typedDataChecks.types === false && <Alert message="Missing types" type="error" />}
+                      {typedDataChecks.message === false && <Alert message="Missing message" type="error" />}
+                      {!invalidJson && !typedDataChecks.hash && (
+                        <Alert message="Invalid EIP-712 input data" type="error" />
+                      )}
+                    </Space>
+                  </>
+                )}
+                <Collapse ghost></Collapse>
+                <Space>
                   <Button
                     size="large"
-                    onClick={() => {
-                      setSigning(false);
-                    }}
+                    type="primary"
+                    onClick={action !== "sign" ? signMessage : injectedProvider ? signMessage : loadWeb3Modal}
+                    disabled={
+                      (type === "typedData" && (!typedDataChecks.hash || invalidJson)) ||
+                      (action === "verify" && (!ethers.utils.isAddress(manualAddress) || !manualSignature))
+                    }
+                    loading={signing}
                     style={{ marginTop: 10 }}
                   >
-                    Cancel
+                    {action !== "sign" ? action : injectedProvider ? action : "Connect account to sign"}
                   </Button>
-                )}
-              </Space>
-            </Card>
-          </Col>
+                  {signing && (
+                    <Button
+                      size="large"
+                      onClick={() => {
+                        setSigning(false);
+                      }}
+                      style={{ marginTop: 10 }}
+                    >
+                      Cancel
+                    </Button>
+                  )}
+                </Space>
+              </Card>
+            </Col>
 
-          <Col span={12}>
-            <Tweets
-              address={address}
-              mainnetProvider={mainnetProvider}
-              firebaseConfig={firebaseConfig}
-              events={events}
-            />
-          </Col>
-        </Row>
-        
-        <div className="" style={{ height: "auto", width: "auto", marginTop: 20 }}>
-        {ready ? (
-          <div className="mx-auto mr-1 ml-1">
-            <h6 className="text-yellow-pos font-bold text-3xl mt-5">Select a User to Sign</h6>
-            <br />
-            <Table pagination={{ pageSize: 5 }} columns={columns2} dataSource={dataSource2}
-            rowSelection={rowSelection}
-            />
-            <Footer />
+            <Col span={12}>
+              <Tweets
+                address={address}
+                mainnetProvider={mainnetProvider}
+                firebaseConfig={firebaseConfig}
+                events={events}
+              />
+            </Col>
+          </Row>
+
+          <div className="" style={{ height: "auto", width: "auto", marginTop: 20 }}>
+            {ready ? (
+              <div className="mx-auto mr-1 ml-1">
+                <h6 className="text-yellow-pos font-bold text-3xl mt-5">Select a User to Sign</h6>
+                <br />
+                <Table
+                  pagination={{ pageSize: 5 }}
+                  columns={columns2}
+                  dataSource={dataSource2}
+                  rowSelection={rowSelection}
+                />
+                <Footer />
+              </div>
+            ) : (
+              <div>
+                <Spin className="mt-5" />
+              </div>
+            )}
           </div>
-        ) : (
-          <div>
-            <Spin className="mt-5" />
-          </div>
-        )}
-      </div>
-      </>
+        </>
       ) : (
         <div style={{ margintop: 50 }}>
           <Form
