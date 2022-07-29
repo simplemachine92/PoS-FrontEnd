3a602badc4791d8c1227affb9d70d3bb21bfcce9
Justin Pulley <justinpulley92@gmail.com>
2022-07-29 08:21:26 -0500

    fix: reduce splash delay

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -94,7 +94,7 @@ function App(props) {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
   }, []);
 
   const [injectedProvider, setInjectedProvider] = useState();
