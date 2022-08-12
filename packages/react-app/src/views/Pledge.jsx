5c5e0e8fb818c8e0bc0856003b19746ae7fb8c52
Justin Pulley <justinpulley92@gmail.com>
2022-08-11 20:53:15 -0500

    Fix: bookplate placeholder on pledge page for smoother loading.

--- packages/react-app/src/views/Pledge.jsx
+++ packages/react-app/src/views/Pledge.jsx
@@ -68,7 +68,7 @@ function Pledge({ writeContracts, tx, address, loadWeb3Modal }) {
             />
               {loaded2 ? null : (
               <svg
-              className="px-2 py-2 ml-4 w-1/2 sm:w-1/3 lg:w-1/4 xl:w-3/12 md:py-2 md:px-2 animate-pulse"
+              className="px-2 py-2 ml-4 w-1/2 sm:w-1/2 md:w-1/2 md:py-2 md:px-2 animate-pulse"
               viewBox="0 0 600 830"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
