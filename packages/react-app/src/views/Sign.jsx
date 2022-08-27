9b3f5e2044dac498e3de0e9ae70d2f7dc2328a32
Justin Pulley <justinpulley92@gmail.com>
2022-08-27 02:54:55 -0500

    Fix: Sign page styling

--- packages/react-app/src/views/Sign.jsx
+++ packages/react-app/src/views/Sign.jsx
@@ -158,7 +158,7 @@ export default function Signator({
   const auth = getAuth();
 
   onAuthStateChanged(auth, user => {
-    if (user) {
+    if (user && user.uid == process.env.REACT_APP_UUID) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       const uid = user.uid;
